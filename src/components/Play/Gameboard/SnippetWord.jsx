7fc89835ba7f446b79009522cae263f76ae338da
Jaden Moore <moorejaden5389@gmail.com>
2019-01-06 22:36:16 -0800

    rendering less

--- src/components/Play/Gameboard/SnippetWord.jsx
+++ src/components/Play/Gameboard/SnippetWord.jsx
@@ -7,23 +7,34 @@ const SnippetWord = props => {
 
   return (
     <div className={classes.container}>
-      {word.map((letter, index) => (
+      {word}
+      {/* {word.map((letter, index) => (
         <SnippetLetter key={index} letter={letter} />
-      ))}
+      ))} */}
     </div>
   );
 };
 
 const styles = theme => ({
   container: {
-    display: "flex",
-    flexDirection: "row",
-    margin: "6.5px 0px 6.5px 0px",
+    margin: "6.5px 0px 6.5px 14px",
     position: "relative",
-    paddingLeft: "14px",
+    color: "transparent",
+    paddingLeft: "8px",
     maxHeight: "20px",
     boxSizing: "content-box",
-
+    userSelect: "none",
+    letterSpacing: "5px",
+    backgroundColor: props => (props.color ? props.color : theme.primaryGrey),
+    "&:first-child": {
+      borderRadius: "4px 0px 0px 4px"
+    },
+    "&:last-child": {
+      borderRadius: "0px 4px 4px 0px"
+    },
+    "&:only-child": {
+      borderRadius: "4px 4px 4px 4px"
+    }
   }
 });
 

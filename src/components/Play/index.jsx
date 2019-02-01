36a0369df90b476193ad95b5e3f0d0e7bf088890
Jdyn <moorejaden5389@gmail.com>
2019-01-31 22:50:58 -0800

    remove code

--- src/components/Play/index.jsx
+++ src/components/Play/index.jsx
@@ -29,44 +29,6 @@ const Play = props => {
     };
   }, []);
 
-  useEffect(
-    () => {
-      updateHeader();
-    },
-    [room.roomTime, socket]
-  );
-
-  const timeRemaining = () => {
-    const roomTime = room.roomTime;
-    if (roomTime) {
-      const text = roomTime.substring(roomTime.length - 2, roomTime.length);
-      const seconds = parseInt(text);
-      return seconds;
-    }
-  };
-
-  const updateHeader = () => {
-    const time = timeRemaining();
-
-    if (time > 10) {
-      setHeader({ color: "#469cd0", text: "Looking for Players..." });
-    } else if (time > 5) {
-      setHeader({ color: "#e57373", text: "Get Ready..." });
-    } else if (time > 0) {
-      setHeader({ color: "#e5a03e", text: "Get Set..." });
-    } else if (time === 0) {
-      setHeader({ color: "#81C784", text: "GO!" });
-    } else if (socket.connected) {
-      setHeader({ color: "#469cd0", text: "Looking for Players..." });
-      return;
-    } else if (socket.errored) {
-      setHeader({ color: "#e57373", text: "Connection error occured" });
-      return;
-    } else {
-      setHeader({ color: "#469cd0", text: "Connecting to server..." });
-    }
-  };
-
   return (
     <main>
       <div className={classes.stripe} />

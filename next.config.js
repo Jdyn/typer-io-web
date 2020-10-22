b16c0dbe5895c2de1720d8f48719655895428eb6
Jaden <moorejaden5389@gmail.com>
2020-10-21 23:11:19 -0700

    use prod

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   reactStrictMode: true,
   env: {
-    API_URL: false ? 'https://typer-io-server.herokuapp.com/api/v1' : 'http://localhost:4000/api/v1',
-    SOCKET_URL: false ? 'https://typer-io-node.herokuapp.com' : 'localhost:8000'
+    API_URL: true ? 'https://typer-io-server.herokuapp.com/api/v1' : 'http://localhost:4000/api/v1',
+    SOCKET_URL: true ? 'https://typer-io-node.herokuapp.com' : 'localhost:8000'
   }
 };

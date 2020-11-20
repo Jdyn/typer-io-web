401d7c344dcb4e2021ea506a702ba43086a046a7
Jaden <moorejaden5389@gmail.com>
2020-11-19 20:28:16 -0800

    update time

--- components/Home/index.tsx
+++ components/Home/index.tsx
@@ -39,7 +39,7 @@ const Home = (): JSX.Element => {
           <div className={styles.newsContainer}>
             <div className={styles.newsWrapper}>
               <h2>Improve your typing speed and race your friends!</h2>
-              <span>updated {formatTime(1605819837331)} </span>
+              <span>updated {formatTime(1605846483477)} </span>
               <p>
                 Thank you for visiting my website! If you have any suggestions,
                 let me know on the forums!

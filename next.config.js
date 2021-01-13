8e1609d7a5d00c6cf791c04474544bf889e33c36
Jaden <moorejaden5389@gmail.com>
2021-01-13 06:42:10 -0800

    add PWA support

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
-module.exports = {
+/* eslint-disable @typescript-eslint/no-var-requires */
+const withPWA = require('next-pwa');
+const runtimeCaching = require('next-pwa/cache');
+
+module.exports = withPWA({
+  pwa: {
+    dest: 'public',
+    runtimeCaching
+  },
   reactStrictMode: true,
   env: {
     BASE_URL: true ? 'https://typer.io' : 'http://localhost:3000',
@@ -7,4 +15,4 @@ module.exports = {
       : 'http://localhost:4000/api/v1',
     SOCKET_URL: true ? 'https://typer-io-node.herokuapp.com' : 'localhost:8000'
   }
-};
+});

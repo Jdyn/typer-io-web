a4eab44b9c6fbdfaa826daa9948bcb6076afa0ba
Jdyn <moorejaden5389@gmail.com>
2021-03-22 01:15:27 -0700

    Create 404.tsx

---
+++ pages/404.tsx
@@ -0,0 +1,22 @@
+import Layout from '../components/Shared/Layout';
+
+const Error404 = () => {
+  return (
+    <Layout striped>
+      <main
+        style={{
+          display: 'flex',
+          margin: '145px 0 0 0',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center'
+        }}
+      >
+        <h1>Oops, a 404 error has occured.</h1>
+        <p>The page you are looking for could not be found.</p>
+      </main>
+    </Layout>
+  );
+};
+
+export default Error404;

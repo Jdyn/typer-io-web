487dbe11aacda1908a3db39febe082b361f687f5
Jaden <moorejaden5389@gmail.com>
2021-04-12 18:16:46 -0700

    Update index.tsx

--- components/Shared/Adsense/index.tsx
+++ components/Shared/Adsense/index.tsx
@@ -28,7 +28,7 @@ const Adsense = (props: Props): JSX.Element => {
     wrapperStyles
   } = props;
 
-  const [display, setDisplay] = useState(true);
+  const [display, setDisplay] = useState(false);
 
   useEffect(() => {
     try {

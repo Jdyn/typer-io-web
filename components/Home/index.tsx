bd6071e11dda981a9c9ced5063b0cb6f417adc07
Jaden <moorejaden5389@gmail.com>
2021-04-18 13:57:09 -0700

    fix banner ad

--- components/Home/index.tsx
+++ components/Home/index.tsx
@@ -32,108 +32,108 @@ const cards = [
 const Home = (): JSX.Element => {
   return (
     <main className={styles.root}>
-      <section className={styles.leftColumn}>
-        <section style={{ maxHeight: '800px' }}>
-          <Adsense
-            client="ca-pub-3148839588626786"
-            slot="7114370741"
-            style={{ display: 'block', textAlign: 'center' }}
-            format="fluid"
-            layout="in-article"
-          />
-        </section>
-        <Paper title="News">
-          <div className={styles.newsContainer}>
-            <div className={styles.newsWrapper}>
-              <h2>Improve your typing speed and race your friends!</h2>
-              <span>updated {formatTime(1618778351949)} </span>
-              <p>
-                Hello, thank you for using my site. if you have any suggestions,
-                please let me know on the forums!
-              </p>
-              <a
-                className={styles.support}
-                href="https://www.buymeacoffee.com/typer"
-                target="_blank"
-                rel="noreferrer"
-              >
-                support the creator ➜
-              </a>
-              <h3>Changes (April 18th, 2021):</h3>
-              <div className={styles.list}>
-                <ul>
-                  <li>
-                    The daily hiscores on the homepage refresh instantly now.
-                    You no longer need to refresh the page.
-                  </li>
-                  <li>
-                    You can now change your username in your profile settings.
-                  </li>
-                  <li>
-                    Custom lobbies can now go up to 60 players, up from 30.
-                  </li>
-                  <li>I have finally added dark mode.</li>
-                </ul>
+      <div className={styles.container}>
+        <section className={styles.leftColumn}>
+          <section>
+            <Adsense
+              client="ca-pub-3148839588626786"
+              slot="7114370741"
+              style={{ display: 'block', textAlign: 'center' }}
+              format="fluid"
+              layout="in-article"
+            />
+          </section>
+          <Paper title="News">
+            <div className={styles.newsContainer}>
+              <div className={styles.newsWrapper}>
+                <h2>Improve your typing speed and race your friends!</h2>
+                <span>updated {formatTime(1618778351949)} </span>
+                <p>
+                  Hello, thank you for using my site. if you have any
+                  suggestions, please let me know on the forums!
+                </p>
+                <a
+                  className={styles.support}
+                  href="https://www.buymeacoffee.com/typer"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  support the creator ➜
+                </a>
+                <h3>Changes (April 18th, 2021):</h3>
+                <div className={styles.list}>
+                  <ul>
+                    <li>
+                      The daily hiscores on the homepage refresh instantly now.
+                      You no longer need to refresh the page.
+                    </li>
+                    <li>
+                      You can now change your username in your profile settings.
+                    </li>
+                    <li>
+                      Custom lobbies can now go up to 60 players, up from 30.
+                    </li>
+                    <li>I have finally added dark mode.</li>
+                  </ul>
+                </div>
               </div>
             </div>
+          </Paper>
+          <section>
+            <Adsense
+              client="ca-pub-3148839588626786"
+              slot="7924266447"
+              style={{ display: 'block', textAlign: 'center' }}
+              format="fluid"
+              layout="in-article"
+            />
+          </section>
+        </section>
+        <section className={styles.centerColumn}>
+          <Profile />
+          <div className={styles.menu}>
+            {cards.map((card) => {
+              return (
+                <Link prefetch={false} href={card.route} key={card.route}>
+                  <a
+                    type="button"
+                    className={`${styles.card} ${styles[card.color]}`}
+                    key={card.route}
+                  >
+                    <h3>{card.title} ➜</h3>
+                    <p>{card.text}</p>
+                  </a>
+                </Link>
+              );
+            })}
           </div>
-        </Paper>
-        <section style={{ maxHeight: '800px' }}>
-          <Adsense
-            client="ca-pub-3148839588626786"
-            slot="7924266447"
-            style={{ display: 'block', textAlign: 'center' }}
-            format="fluid"
-            layout="in-article"
-          />
+          <Hiscores />
         </section>
-      </section>
-      <section className={styles.centerColumn}>
-        <Profile />
-        <div className={styles.menu}>
-          {cards.map((card) => {
-            return (
-              <Link prefetch={false} href={card.route} key={card.route}>
-                <a
-                  type="button"
-                  className={`${styles.card} ${styles[card.color]}`}
-                  key={card.route}
-                >
-                  <h3>{card.title} ➜</h3>
-                  <p>{card.text}</p>
-                </a>
-              </Link>
-            );
-          })}
-        </div>
-        <Hiscores />
-      </section>
-      <section className={styles.rightColumn}>
-        <RecentPosts />
-        <section style={{ maxHeight: '300px' }}>
-          <Adsense
-            client="ca-pub-3148839588626786"
-            slot="8439040257"
-            style={{ display: 'block', textAlign: 'center' }}
-            format="fluid"
-            layout="in-article"
-          />
+        <section className={styles.rightColumn}>
+          <RecentPosts />
+          <section>
+            <Adsense
+              client="ca-pub-3148839588626786"
+              slot="8439040257"
+              style={{ display: 'block', textAlign: 'center' }}
+              format="fluid"
+              layout="in-article"
+            />
+          </section>
+          <RecentMatches />
         </section>
-        <RecentMatches />
-      </section>
+      </div>
       <section
-        className={styles.ad}
         style={{
-          maxHeight: '200px',
           maxWidth: '780px',
-          margin: '0 auto',
-          gridArea: 'ad'
+          margin: '20px auto',
+          padding: '0px 10px'
         }}
       >
         <Adsense
           client="ca-pub-3148839588626786"
           slot="7924266447"
-          style={{ display: 'block', textAlign: 'center' }}
+          style={{ display: 'block', width: '100%' }}
           format="fluid"
           layout="in-article"
         />

0181979fc140bc7c11c2c6c6f0656662e7f6bef9
Jaden <moorejaden5389@gmail.com>
2021-06-19 13:46:43 -0700

    use subdomains

--- next.config.js
+++ next.config.js
@@ -2,10 +2,10 @@
 /* eslint-disable no-param-reassign */
 const basePath = '';
 
-const PROD_API_URL = 'https://typer-io-server.herokuapp.com/api';
+const PROD_API_URL = 'https://api.typer.io/api';
 const DEV_API_URL = 'http://localhost:4000/api';
 
-const PROD_SOCKET_URL = 'https://typer-io-node.herokuapp.com';
+const PROD_SOCKET_URL = 'https://app.typer.io';
 const DEV_SOCKET_URL = 'localhost:8000';
 
 const PROD_BASE_URL = 'https://typer.io';

45e49de27e14bb18038f5fcd0e92863043b84c8d
Jaden <moorejaden5389@gmail.com>
2023-01-09 21:20:50 -0800

    fix searching

--- components/Account/Search/Search.tsx
+++ components/Account/Search/Search.tsx
@@ -6,37 +6,38 @@ import { useSearchUserQuery } from '../../../services/account';
 import styles from './Search.module.css';
 import formatTime from '../../../util/formatTime';
 import Paginate from '../../Shared/Paginate';
-import useNextQueryParams from '../../../util/useNextQueryParam';
 
-const SearchPage = (): JSX.Element => {
+interface SearchProps {
+  term: string | string[];
+  page: string | string[];
+}
+
+const SearchPage = ({ term, page }: SearchProps): JSX.Element => {
   const router = useRouter();
-  const { user, page } = useNextQueryParams();
 
   const { data: userPage, isSuccess } = useSearchUserQuery({
-    username: user as string,
+    username: term as string,
     page: page as string
   });
 
   return (
     <main className={styles.root}>
-      <Paper title={`Search Results - ${user as string}`}>
+      <Paper title={`Search Results - ${term as string}`}>
         <div className={styles.container}>
           {isSuccess && (
             <div className={styles.wrapper}>
               {userPage?.data?.length > 0 ? (
                 userPage?.data?.map((item) => (
-                  <Link key={item.username} href={`/u/${item.username}`}>
-                    <div className={styles.entry}>
-                      <div>
-                        <span className={styles.nameLink}>{item.username}</span>
-                        {item.isAdmin && <span className={styles.admin}>Creator</span>}
-                      </div>
-                      <span>
-                        {item.country && `${item.country} - `}
-                        {`Joined ${formatTime(item.insertedAt)}`}
-                      </span>
-                      {item.bio && <p>{item.bio}</p>}
+                  <Link className={styles.entry} key={item.username} href={`/u/${item.username}`}>
+                    <div>
+                      <span className={styles.nameLink}>{item.username}</span>
+                      {item.isAdmin && <span className={styles.admin}>Creator</span>}
                     </div>
+                    <span>
+                      {item.country && `${item.country} - `}
+                      {`Joined ${formatTime(item.insertedAt)}`}
+                    </span>
+                    {item.bio && <p>{item.bio}</p>}
                   </Link>
                 ))
               ) : (
@@ -46,7 +47,8 @@ const SearchPage = (): JSX.Element => {
           )}
         </div>
         <Paginate
-          pageUpdated={(newPage) => router.push(`/search/${user}?page=${newPage}`)}
+          defaultPage={1}
+          pageUpdated={(newPage) => router.push(`/search/${term}?page=${newPage}`)}
           totalPages={userPage?.totalPages}
         />
       </Paper>
---
+++ pages/search/[term].tsx
@@ -0,0 +1,16 @@
+import { useRouter } from 'next/router';
+import Search from '../../components/Account/Search';
+import Layout from '../../components/Layout';
+import useNextQueryParams from '../../util/useNextQueryParam';
+
+const SearchContainer = (): JSX.Element => {
+  const { term, page } = useRouter().query;
+
+  return (
+    <Layout striped title={`Player Search | ${term}`}>
+      {term && <Search term={term} page={page} />}
+    </Layout>
+  );
+};
+
+export default SearchContainer;
